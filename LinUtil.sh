dd70641ab579a0f6105168cf46fd4a98bf1d3370
Clayton Reardon <plus1059@proton.me>
2024-05-26 00:02:44 -0700

    Fixed busted os_detect function

--- LinUtil.sh
+++ LinUtil.sh
@@ -3,6 +3,7 @@
 detect_os() {
     if [ -f /etc/os-release ]; then
         . /etc/os-release
+        OS=$ID
         case "$ID" in 
             debian|ubuntu)
                 PKG_MAN="nala"
@@ -47,62 +48,73 @@ detect_os() {
 
 update_pkgs() {
     echo -e "${bldgrn}Updating package list...${rst}"
+    detect_os
     case "$ID" in
-    debian|ubuntu)
-        echo -e "${bldgrn}Installing Nala.${rst}"
-        sudo apt update && sudo upgrade -y && sudo apt install nala -y
-        ;;
-    *)
-        $UPGRADE
-        ;;
-    esac    
+        debian|ubuntu)
+            echo -e "${bldgrn}Installing Nala.${rst}"
+            sudo apt update && sudo upgrade -y && sudo apt install nala -y
+            ;;
+        *)
+            $UPGRADE
+            ;;
+        esac    
 }
 
 install_pkgs() {
     echo -e "${bldgrn}Installing common packages${rst}"
     $INSTALL zsh exa unzip curl git
-
+    detect_os
     case "$ID" in
         debian|ubuntu)
-        echo -e "${bldgrn}Installing Debian specific packages${rst}"
-        $INSTALL fd-find ripgrep bat build-essential btop trash-cli
-        ;;
+            echo -e "${bldgrn}Installing Debian specific packages${rst}"
+            $INSTALL fd-find ripgrep bat build-essential btop trash-cli
+            ;;
         arch)
-        echo -e "${bldgrn}Installing Arch specific packages${rst}"
-        $INSTALL fd ripgrep bat btop trash-cli
-        $INSTALL --needed base-devel
-        ;;
+            echo -e "${bldgrn}Installing Arch specific packages${rst}"
+            $INSTALL fd ripgrep bat btop trash-cli
+            $INSTALL --needed base-devel
+            ;;
         fedora)
-        echo -e "${bldgrn}Installing Fedora specific packages${rst}"
-        $INSTALL fd-find ripgrep bat btop trash-cli
-        sudo dnf groupinstall "Development Tools" -y
-        ;;
+            echo -e "${bldgrn}Installing Fedora specific packages${rst}"
+            $INSTALL fd-find ripgrep bat btop trash-cli
+            sudo dnf groupinstall "Development Tools" -y
+            ;;
         opensuse)
-        echo -e "${bldgrn}Installing OpenSUSE specific packages${rst}"
-        $INSTALL fd ripgrep bat btop
-        sudo zypper install --type pattern devel_basis -y
-        ;;
+            echo -e "${bldgrn}Installing OpenSUSE specific packages${rst}"
+            $INSTALL fd ripgrep bat btop
+            sudo zypper install --type pattern devel_basis -y
+            ;;
         rhel)
-        echo -e "${bldgrn}Installing RHEL specific packages${rst}"
-        sudo dnf groupinstall "Development Tools" -y
-        # Install fd-find
-        sudop dnf copr enable tkbcopr/fd -y
-        sudo dnf install fd -f
-        # Install ripgrep
-        sudo yum install yum-utils -y
-        sudo yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
-        sudo yum install ripgrep -y
-        # Install bat
-        wget https://github.com/sharkdp/bat/releases/download/v0.23.0/bat-v0.23.0-x86_64-unknown-linux-gnu.tar.gz
-        tar -xvf bat-v0.23.0-x86_64-unknown-linux-gnu.tar.gz
-        sudo mv bat-v0.23.0-x86_64-unknown-linux-gnu/bat /usr/bin/bat
-        rm -r bat-v0.23.0-x86_64-unknown-linux-gnu.tar.gz
-        # Install btop
-        sudo dnf install epel-release -y
-        sudo dnf install btop -y
+            echo -e "${bldgrn}Installing RHEL specific packages${rst}"
+            sudo dnf groupinstall "Development Tools" -y
+            # Install fd-find
+            sudop dnf copr enable tkbcopr/fd -y
+            sudo dnf install fd -f
+            # Install ripgrep
+            sudo yum install yum-utils -y
+            sudo yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
+            sudo yum install ripgrep -y
+            # Install bat
+            wget https://github.com/sharkdp/bat/releases/download/v0.23.0/bat-v0.23.0-x86_64-unknown-linux-gnu.tar.gz
+            tar -xvf bat-v0.23.0-x86_64-unknown-linux-gnu.tar.gz
+            sudo mv bat-v0.23.0-x86_64-unknown-linux-gnu/bat /usr/bin/bat
+            rm -r bat-v0.23.0-x86_64-unknown-linux-gnu.tar.gz
+            # Install btop
+            sudo dnf install epel-release -y
+            sudo dnf install btop -y
     esac
 }
 
-# Move zshrc and starship.toml to correct location
-cp zshrc ~/.zshrc
-cp starship.toml ~/.config/starship.toml
+main() {
+    detect_os
+    update_pkgs
+    install_pkgs
+    # Move zshrc and starship.toml to correct location
+    cp zshrc ~/.zshrc
+    if [ ! -d ~/.config ]; then
+        mkdir ~/.config
+    fi 
+    cp starship.toml ~/.config/starship.toml
+}
+
+main
